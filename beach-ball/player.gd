1d98d063799c172fcf98e208a8e0260c6c3914e0
Marwan <marwan.abdulaziz.75@gmail.com>
2026-07-03 20:08:34 +0300

    added player movement

--- beach-ball/player.gd
+++ beach-ball/player.gd
@@ -1,35 +1,34 @@
 extends CharacterBody2D
 
-
-
-
-
-
 @export var speed: float = 400.0
 @export var jump_velocity: float = -500.0
 @export var acceleration: float = 2000.0
 @export var friction: float = 1800.0
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	# الجاذبية
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# القفز (استخدام ضغطة واحدة فقط)
-	if Input.is_physical_key_pressed(KEY_UP) and is_on_floor():
+	# القفز
+	if Input.is_action_just_pressed("jump_p") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# حساب الاتجاه يدوياً
-	var direction = 0
-	if Input.is_physical_key_pressed(KEY_RIGHT):
-		direction += 1
-	if Input.is_physical_key_pressed(KEY_LEFT):
-		direction -= 1
-	
+	# الحركة
+	var direction = Input.get_axis("move_left_p", "move_right_p")
+
 	# تطبيق الحركة
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+	# انيماشين - اتشيك بس على الحركة الأفقية
+	if abs(velocity.x) > 0:
+		animation_player.play("run")
+	else:
+		animation_player.play("idel")
 
 	move_and_slide()
--- beach-ball/plyer_2.gd
+++ beach-ball/plyer_2.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-
-
 @export var speed: float = 400.0
 @export var jump_velocity: float = -500.0
 @export var acceleration: float = 2000.0
@@ -12,17 +10,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# القفز بالـ W
-	if Input.is_physical_key_pressed(KEY_W) and is_on_floor():
+	# القفز - just_pressed عشان تحصل مرة واحدة بس
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# الحركة بـ A و D
-	var direction = 0
-	if Input.is_physical_key_pressed(KEY_D):
-		direction += 1
-	if Input.is_physical_key_pressed(KEY_A):
-		direction -= 1
-	
+	# الحركة
+	var direction = Input.get_axis("move_left", "move_right")
+
 	# تطبيق الحركة
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
